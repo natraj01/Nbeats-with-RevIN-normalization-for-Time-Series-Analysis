1997925e138fa0a164bf6816b2caf6097f838ab0
Natraj Birla <95861907+natraj01@users.noreply.github.com>
2022-04-01 01:24:59 +0530

    Create compute_matrics.py

---
+++ compute_matrics.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+
+def mean_absolute_scaled_error(real, pred):
+
+  mae = tf.reduce_mean(tf.abs(real - pred))
+
+  mae_naive_no_season = tf.reduce_mean(tf.abs(real[1:] - real[:-1]))
+
+  return mae / mae_naive_no_season
+
+
+def compute_metrics(actual, predictions):
+    predictions = tf.reshape(predictions,shape=(len(predictions),))
+    actual = tf.reshape(actual, shape=(len(actual),))
+
+    mae = tf.keras.metrics.mean_absolute_error(actual, predictions)
+    mse = tf.keras.metrics.mean_squared_error(actual, predictions)
+    rmse = tf.sqrt(mse)
+    mape = tf.keras.metrics.mean_absolute_percentage_error(actual, predictions)
+
+    return {"mae": mae.numpy(),
+          "mse": mse.numpy(),
+          "rmse": rmse.numpy(),
+          "mape": mape.numpy()}
